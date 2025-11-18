dc4b719a842fe5aa1d0c336cc96c190fec9d582e
Fernando Burgos <fernandoburgos8001@gmail.com>
2025-11-18 19:05:00 +0000

    Fix DATABASE_URL, admin panel and main.py final version

--- app/database.py
+++ app/database.py
@@ -1,20 +1,14 @@
-# app/database.py
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL is missing")
+
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
